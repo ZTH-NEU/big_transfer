311410c48ccb9d6bb06eb11e37c7d2427f36b8b7
ZTH-NEU <zhaotianhu1996@163.com>
2021-10-13 23:20:23 +0800

    'roc 模型'

--- bit_pytorch/ROC.py
+++ bit_pytorch/ROC.py
@@ -1,116 +1,58 @@
-import torch
-import torch.nn as nn
-import os
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 21 16:13:04 2017
+@author: lizhen
+"""
 import numpy as np
-from torchvision.datasets import ImageFolder
-from scipy import interp
 import matplotlib.pyplot as plt
-from itertools import cycle
-from sklearn.metrics import roc_curve, auc, f1_score, precision_recall_curve, average_precision_score
+from sklearn import svm, datasets
+from sklearn.metrics import roc_curve, auc  ###计算roc和auc
 
-os.environ['CUDA_VISIBLE_DEVICES'] = "0"
+from sklearn.model_selection import KFold
 
-data_root = r'D:\TJU\GBDB\set113\set113_images\test1'  # 测试集路径
-test_weights_path = r"C:\Users\admin\Desktop\fsdownload\epoch_0278_top1_70.565_'checkpoint.pth.tar'"  # 预训练模型参数
-num_class = 113  # 类别数量
-gpu = "cuda:0"
+from sklearn.model_selection import train_test_split
 
 
-# mean=[0.948078, 0.93855226, 0.9332005], var=[0.14589554, 0.17054074, 0.18254866]
-def test(model, test_path):
-    # 加载测试集和预训练模型参数
-    test_dir = os.path.join(data_root, 'test_images')
-    class_list = list(os.listdir(test_dir))
-    class_list.sort()
-    transform_test = get_transform_for_test(mean=[0.948078, 0.93855226, 0.9332005],
-                                            var=[0.14589554, 0.17054074, 0.18254866])
-    test_dataset = ImageFolder(test_dir, transform=transform_test)
-    test_loader = torch.utils.data.DataLoader(
-        test_dataset, batch_size=1, shuffle=False, drop_last=False, pin_memory=True, num_workers=1)
-    checkpoint = torch.load(test_path)
-    model.load_state_dict(checkpoint['state_dict'])
-    model.eval()
+# Import some data to play with
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
 
-    score_list = []  # 存储预测得分
-    label_list = []  # 存储真实标签
-    for i, (inputs, labels) in enumerate(test_loader):
-        inputs = inputs.cuda()
-        labels = labels.cuda()
+##变为2分类
+X, y = X[y != 2], y[y != 2]
 
-        outputs = model(inputs)
-        # prob_tmp = torch.nn.Softmax(dim=1)(outputs) # (batchsize, nclass)
-        score_tmp = outputs  # (batchsize, nclass)
+print(X)
+print(y)
 
-        score_list.extend(score_tmp.detach().cpu().numpy())
-        label_list.extend(labels.cpu().numpy())
+# Add noisy features to make the problem harder
+random_state = np.random.RandomState(0)
+n_samples, n_features = X.shape
+X = np.c_[X, random_state.randn(n_samples, 200 * n_features)]
 
-    score_array = np.array(score_list)
-    # 将label转换成onehot形式
-    label_tensor = torch.tensor(label_list)
-    label_tensor = label_tensor.reshape((label_tensor.shape[0], 1))
-    label_onehot = torch.zeros(label_tensor.shape[0], num_class)
-    label_onehot.scatter_(dim=1, index=label_tensor, value=1)
-    label_onehot = np.array(label_onehot)
+# shuffle and split training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=.3,random_state= None)
 
-    print("score_array:", score_array.shape)  # (batchsize, classnum)
-    print("label_onehot:", label_onehot.shape)  # torch.Size([batchsize, classnum])
+# Learn to predict each class against the other
+svm = svm.SVC(kernel='linear', probability=True, random_state=random_state)
 
-    # 调用sklearn库，计算每个类别对应的fpr和tpr
-    fpr_dict = dict()
-    tpr_dict = dict()
-    roc_auc_dict = dict()
-    for i in range(num_class):
-        fpr_dict[i], tpr_dict[i], _ = roc_curve(label_onehot[:, i], score_array[:, i])
-        roc_auc_dict[i] = auc(fpr_dict[i], tpr_dict[i])
-    # micro
-    fpr_dict["micro"], tpr_dict["micro"], _ = roc_curve(label_onehot.ravel(), score_array.ravel())
-    roc_auc_dict["micro"] = auc(fpr_dict["micro"], tpr_dict["micro"])
+###通过decision_function()计算得到的y_score的值，用在roc_curve()函数中
+y_score = svm.fit(X_train, y_train).decision_function(X_test)
+print(y_test)
+print(y_score)
+# Compute ROC curve and ROC area for each class
+fpr, tpr, threshold = roc_curve(y_test, y_score)  ###计算真正率和假正率
+roc_auc = auc(fpr, tpr)  ###计算auc的值
 
-    # macro
-    # First aggregate all false positive rates
-    all_fpr = np.unique(np.concatenate([fpr_dict[i] for i in range(num_class)]))
-    # Then interpolate all ROC curves at this points
-    mean_tpr = np.zeros_like(all_fpr)
-    for i in range(num_class):
-        mean_tpr += interp(all_fpr, fpr_dict[i], tpr_dict[i])
-    # Finally average it and compute AUC
-    mean_tpr /= num_class
-    fpr_dict["macro"] = all_fpr
-    tpr_dict["macro"] = mean_tpr
-    roc_auc_dict["macro"] = auc(fpr_dict["macro"], tpr_dict["macro"])
-
-    # 绘制所有类别平均的roc曲线
-    plt.figure()
-    lw = 2
-    plt.plot(fpr_dict["micro"], tpr_dict["micro"],
-             label='micro-average ROC curve (area = {0:0.2f})'
-                   ''.format(roc_auc_dict["micro"]),
-             color='deeppink', linestyle=':', linewidth=4)
-
-    plt.plot(fpr_dict["macro"], tpr_dict["macro"],
-             label='macro-average ROC curve (area = {0:0.2f})'
-                   ''.format(roc_auc_dict["macro"]),
-             color='navy', linestyle=':', linewidth=4)
-
-    colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
-    for i, color in zip(range(num_class), colors):
-        plt.plot(fpr_dict[i], tpr_dict[i], color=color, lw=lw,
-                 label='ROC curve of class {0} (area = {1:0.2f})'
-                       ''.format(i, roc_auc_dict[i]))
-    plt.plot([0, 1], [0, 1], 'k--', lw=lw)
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Some extension of Receiver operating characteristic to multi-class')
-    plt.legend(loc="lower right")
-    plt.savefig('set113_roc.jpg')
-    plt.show()
-
-
-if __name__ == '__main__':
-    # 加载模型
-    seresnet = FineTuneSEResnet50(num_class=num_class)
-    device = torch.device(gpu)
-    seresnet = seresnet.to(device)
-    test(seresnet, test_weights_path)
+plt.figure()
+lw = 2
+plt.figure(figsize=(10, 10))
+plt.plot(fpr, tpr, color='darkorange',
+         lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)  ###假正率为横坐标，真正率为纵坐标做曲线
+plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic example')
+plt.legend(loc="lower right")
+plt.show()
